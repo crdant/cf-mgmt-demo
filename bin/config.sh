f863000b11963c9f9b3960d8816fc13b4b2eee6b
Chuck D'Antonio <c_dantonio@acm.org>
2017-08-21 12:41:41 -0400

    Updated 'crew' to 'sailors' because that's what I named it

--- bin/config.sh
+++ bin/config.sh
@@ -8,25 +8,25 @@ cf-mgmt init-config
 cf-mgmt add-org-to-config --org navy --org-billing-mgr-grp gentry --org-auditor-grp captain
 cf-mgmt add-space-to-config --org navy --space production --space-mgr-grp captains --space-auditor-grp midshipman
 cf-mgmt add-space-to-config --org navy --space integration --space-mgr-grp lieutenants --space-auditor-grp sailors
-cf-mgmt add-space-to-config --org navy --space development --space-mgr-grp crew --space-dev-grp sailors
+cf-mgmt add-space-to-config --org navy --space development --space-mgr-grp sailors --space-dev-grp sailors
 
 # HMS Victory applications
 cf-mgmt add-org-to-config --org victory --org-billing-mgr-grp gentry --org-auditor-grp "HMS Victory"
 cf-mgmt add-space-to-config --org victory --space production --space-mgr-grp captains --space-auditor-grp "HMS Victory"
 cf-mgmt add-space-to-config --org victory --space integration --space-mgr-grp lieutenants --space-auditor-grp "HMS Victory"
-cf-mgmt add-space-to-config --org victory --space development --space-mgr-grp crew --space-dev-grp "HMS Victory"
+cf-mgmt add-space-to-config --org victory --space development --space-mgr-grp sailors --space-dev-grp "HMS Victory"
 
 # HMS Bounty applications
 cf-mgmt add-org-to-config --org bounty --org-billing-mgr-grp gentry --org-auditor-grp "HMS Bounty"
 cf-mgmt add-space-to-config --org bounty --space production --space-mgr-grp captains --space-auditor-grp "HMS Bounty"
 cf-mgmt add-space-to-config --org bounty --space integration --space-mgr-grp lieutenants --space-auditor-grp "HMS Bounty"
-cf-mgmt add-space-to-config --org bounty --space development --space-mgr-grp crew --space-dev-grp "HMS Bounty"
+cf-mgmt add-space-to-config --org bounty --space development --space-mgr-grp sailors --space-dev-grp "HMS Bounty"
 
 # HMS Lydia applications
 cf-mgmt add-org-to-config --org lydia --org-billing-mgr-grp gentry --org-auditor-grp "HMS Lydia"
 cf-mgmt add-space-to-config --org lydia --space production --space-mgr-grp captains --space-auditor-grp "HMS Lydia"
 cf-mgmt add-space-to-config --org lydia --space integration --space-mgr-grp lieutenants --space-auditor-grp "HMS Lydia"
-cf-mgmt add-space-to-config --org lydia --space development --space-mgr-grp crew --space-dev-grp "HMS Lydia"
+cf-mgmt add-space-to-config --org lydia --space development --space-mgr-grp sailors --space-dev-grp "HMS Lydia"
 
 # modify ldap configuration
 sed -i '' 's#enabled: false#enabled: true#' ${BASEDIR}/config/ldap.yml
